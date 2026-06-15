316758a7199ce0dac3e54de92367dddfb4820217
yaoyibin.vi <yaoyibin.vi@bytedance.com>
2026-06-16 00:39:44 +0800

    feat(sleep-music): 装 @mediagrid/capacitor-native-audio v3 + iOS/安卓后台音频配置(cap sync)
    Change-Id: I9867493e40807f1b3bb5853cc67f6b313d2feab2

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -15,7 +15,8 @@ let package = Package(
         .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
         .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics"),
         .package(name: "CapacitorLocalNotifications", path: "../../../node_modules/@capacitor/local-notifications"),
-        .package(name: "CapgoCapacitorUpdater", path: "../../../node_modules/@capgo/capacitor-updater")
+        .package(name: "CapgoCapacitorUpdater", path: "../../../node_modules/@capgo/capacitor-updater"),
+        .package(name: "MediagridCapacitorNativeAudio", path: "../../../node_modules/@mediagrid/capacitor-native-audio")
     ],
     targets: [
         .target(
@@ -26,7 +27,8 @@ let package = Package(
                 .product(name: "CapacitorApp", package: "CapacitorApp"),
                 .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
                 .product(name: "CapacitorLocalNotifications", package: "CapacitorLocalNotifications"),
-                .product(name: "CapgoCapacitorUpdater", package: "CapgoCapacitorUpdater")
+                .product(name: "CapgoCapacitorUpdater", package: "CapgoCapacitorUpdater"),
+                .product(name: "MediagridCapacitorNativeAudio", package: "MediagridCapacitorNativeAudio")
             ]
         )
     ]
